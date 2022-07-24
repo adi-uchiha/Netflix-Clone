ffa144d1eb869bfc56729a97dd1bf3c1527b7aec
Aditya Shelke <adi.cs.night@gmail.com>
2022-07-24 13:29:36 +0530

    SHort change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./home/Home";
 import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Navbar/>
     </div>
   );
 }
